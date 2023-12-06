82867b25cf23e146db618018d72a9440f47b0e61
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2023-12-06 20:21:31 +0100

    tests

--- src/test.py
+++ src/test.py
@@ -36,7 +36,7 @@ def testAll():
     retrieval_system.tfidf_matrix = retrieval_system.vectorize_documents(documents)
 
     metrics = {"precisions": [], "recalls": []}
-    for title, similar_documents_gt in alive_it(ground_truth_labels.items(), "Testing"):
+    for title, similar_documents_gt in alive_it(ground_truth_labels.items(), title= "Testing"):
         query_document = doc_dict[title]
         similar_documents_titles, similar_documents = retrieval_system.retrieve_similar_documents(query_document, title, 10)
         par = calculatePrecisionAndRecall(similar_documents_gt, similar_documents_titles)
