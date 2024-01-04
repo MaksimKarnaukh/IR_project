40a22923dbf7e544f2c0b0c186d7927598a6e44e
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2024-01-04 18:50:28 +0100

    Changes by Billy

--- src/test.py
+++ src/test.py
@@ -27,6 +27,11 @@ def calculatePrecisionAndRecall(expected, retrieved) -> tuple:
     return precision, recall
 
 
+def calculateF1score(precision, recall) -> float:
+    F1_score = (2*precision*recall) + (precision+recall)
+    return F1_score
+
+
 def testAll():
     ground_truth_labels: dict = read_gt(variables.filepath_path_gt)
     doc_dict = getDocDict(filepath_video_games=variables.filepath_video_games, csv_doc_dict=variables.csv_doc_dict)
@@ -35,16 +40,19 @@ def testAll():
     retrieval_system = RelatedDocumentsRetrieval(document_titles, documents)
     retrieval_system.tfidf_matrix = retrieval_system.vectorize_documents(documents)
 
-    metrics = {"precisions": [], "recalls": []}
+    metrics = {"precisions": [], "recalls": [], "F1 scores": []}
     for title, similar_documents_gt in alive_it(ground_truth_labels.items(), title= "Testing"):
         query_document = doc_dict[title]
         similar_documents_titles, similar_documents = retrieval_system.retrieve_similar_documents(query_document, title, 10)
         par = calculatePrecisionAndRecall(similar_documents_gt, similar_documents_titles)
         metrics["precisions"].append(par[0])
         metrics["recalls"].append(par[1])
+        F1 = calculateF1score(par[0], par[1])
+        metrics["F1 scores"].append(F1)
     print('\n')
     print("Average precision: ", sum(metrics["precisions"])/len(metrics["precisions"]))
     print("Average recall: ", sum(metrics["recalls"])/len(metrics["recalls"]))
+    print("Average F1 score: ", sum(metrics["F1 scores"])/len(metrics["F1 scores"]))
 
 if __name__ == '__main__':
     # d = read_gt(variables.filepath_path_gt)  # Read ground truth
