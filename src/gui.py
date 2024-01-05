268cd2fe9e950175630b4a622fb0f4829a44aa7b
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2024-01-05 20:02:10 +0100

    gui v2

--- src/gui.py
+++ src/gui.py
@@ -7,52 +7,31 @@ from related_doc_retrieval import *
 from src import variables
 
 
-def return_similar_documents(doc_dict, query: str, by_title: bool = False, num_results: int = 10):
-
-    documents = list(doc_dict.values())
-    document_titles = list(doc_dict.keys())
-
-    retrieval_system = RelatedDocumentsRetrieval(document_titles, documents)
-
-    # check if tfidf_matrix.csv exists
-    retrieval_system._tfidf_matrix = None
-    if not os.path.isfile(variables.tfidf_matrix_csv_path):
-        retrieval_system._tfidf_matrix = retrieval_system.vectorize_documents()
-        store_tfidf_matrix(retrieval_system._tfidf_matrix)
-    else:
-        retrieval_system._tfidf_matrix = load_tfidf_matrix()
-        retrieval_system.own_vectorizer.fit_transform(documents, retrieval_system._tfidf_matrix)
-
-    similar_documents_titles, similar_documents = [], []
-    if by_title:
-        query_document = doc_dict[query]
-        similar_documents_titles, similar_documents, scores = retrieval_system.retrieve_similar_documents(query_document, query, num_results)
-    else:
-        similar_documents_titles, similar_documents, scores = retrieval_system.retrieve_similar_documents(query, "", num_results)
-
-    print("Query Document:", query)
-    print("Similar Documents:")
-    for i, doc in enumerate(similar_documents_titles, 1):
-        print(f"{i}. {doc}")
-
-    if by_title:
-        title = query
-        d = read_gt(variables.filepath_path_gt)  # Read ground truth
-        print(d[title])
-
-        expected = list(d[title].keys())
-        par = calculatePrecisionAndRecall(expected, similar_documents_titles)
-        print("fraction of relevant instances among the retrieved instances: ", par[0])
-        print("fraction of relevant instances that were retrieved: ", par[1])
-
-        return similar_documents_titles, similar_documents, par[0], par[1]
-
-    return similar_documents_titles, similar_documents
-
-
 class SimpleGUI:
 
-    def center_window(self, window_width=650, window_height=650):
+    def return_similar_documents(self, doc_dict, query: str, by_title: bool = False, num_results: int = 10):
+
+        similar_documents_titles, similar_documents = [], []
+        if by_title:
+            query_document = doc_dict[query]
+            similar_documents_titles, similar_documents, scores = self.retrieval_system.retrieve_similar_documents(
+                query_document, query, num_results)
+        else:
+            similar_documents_titles, similar_documents, scores = self.retrieval_system.retrieve_similar_documents(query, "",
+                                                                                                              num_results)
+        if by_title:
+            title = query
+            d = read_gt(variables.filepath_path_gt)  # Read ground truth
+            print(d[title])
+
+            expected = list(d[title].keys())
+            par = calculatePrecisionAndRecall(expected, similar_documents_titles)
+
+            return similar_documents_titles, similar_documents, par[0], par[1]
+
+        return similar_documents_titles, similar_documents
+
+    def center_window(self, window_width=650, window_height=700):
         """
         Center the window on the screen.
         :param window_width: window width
@@ -80,8 +59,11 @@ class SimpleGUI:
 
         if selected_option == "Query Sentence":
             input_text = self.entry.get()
-            output_text = ""
-            result = return_similar_documents(self.doc_dict, input_text, by_title=False, num_results=10)
+            # check if self.num_results_entry.get() is a number in string format
+            num_results = 10
+            if self.num_results_entry.get().isdigit():
+                num_results = int(self.num_results_entry.get())
+            result = self.return_similar_documents(self.doc_dict, input_text, by_title=False, num_results=num_results)
             if len(result) == 4:
                 similar_documents_titles, similar_documents, par0, par1 = result
                 # output_text1 = f"Query Sentence: {input_text}\n" \
@@ -101,17 +83,21 @@ class SimpleGUI:
                 output_text1 = f"Similar Documents:\n"
 
                 for i, doc in enumerate(similar_documents_titles, 1):
-                    output_text += f"{i}. {doc}\n"
+                    output_text1 += f"{i}. {doc}\n"
                 self.output_box1.insert(tk.END, output_text1 + "\n")
 
-            # output_text = f"You entered: {input_text}"
-            self.output_box1.insert(tk.END, output_text + "\n")
+            # output_text = f"You entered: {num_results}"
+            # self.output_box1.insert(tk.END, output_text + "\n")
+
             self.entry.delete(0, tk.END)
+
         elif selected_option == "Title":
             selected_title = self.title_combobox.get()
             if selected_title:
-                output_text = ""
-                result = return_similar_documents(self.doc_dict, selected_title, by_title=True, num_results=10)
+                num_results = 10
+                if self.num_results_entry.get().isdigit():
+                    num_results = int(self.num_results_entry.get())
+                result = self.return_similar_documents(self.doc_dict, selected_title, by_title=True, num_results=num_results)
                 if len(result) == 4:
                     similar_documents_titles, similar_documents, par0, par1 = result
                     # output_text1 = f"Title: {selected_title}\n" \
@@ -135,9 +121,16 @@ class SimpleGUI:
                 # output_text = f"You selected the title: {selected_title}"
                 # self.output_box1.insert(tk.END, output_text + "\n")
 
-    def __init__(self, root, doc_dict):
+    def __init__(self, root, doc_dict, retrieval_system):
 
         self.doc_dict = doc_dict
+        self.documents = list(doc_dict.values())
+        self.document_titles = list(doc_dict.keys())
+
+        self.retrieval_system = retrieval_system
+
+
+
         self.root = root
         root.title("Similar Documents Retrieval System")
 
@@ -164,9 +157,14 @@ class SimpleGUI:
         self.entry = ttk.Entry(root, font=("Helvetica", 14))
         self.entry.pack(pady=10)
 
-        # A list of titles (for testing)
-        document_titles = list(doc_dict.keys())
-        titles = document_titles
+        # Additional Input for Number of Results
+        self.num_results_label = ttk.Label(root, text="Number of Results:", font=("Helvetica", 16))
+        self.num_results_label.pack()
+        self.num_results_entry = ttk.Entry(root, font=("Helvetica", 14))
+        self.num_results_entry.pack(pady=10)
+
+        # A list of titles
+        titles = self.document_titles
 
         # Create a title Combobox
         self.title_combobox = ttk.Combobox(root, values=titles, font=("Helvetica", 14), state="readonly")
@@ -177,13 +175,13 @@ class SimpleGUI:
         self.process_button.pack(pady=15)
 
         # Output Text Box
-        self.output_label1 = ttk.Label(root, text="Output:", font=("Helvetica", 16))
+        self.output_label1 = ttk.Label(root, text="Similar documents:", font=("Helvetica", 16))
         self.output_label1.pack()
         self.output_box1 = tk.Text(root, height=10, width=60, wrap=tk.WORD, font=("Helvetica", 12))
         self.output_box1.pack(padx=10, pady=5)
 
         # Output Text Box
-        self.output_label2 = ttk.Label(root, text="Output:", font=("Helvetica", 16))
+        self.output_label2 = ttk.Label(root, text="Performance statistics:", font=("Helvetica", 16))
         self.output_label2.pack()
         self.output_box2 = tk.Text(root, height=5, width=60, wrap=tk.WORD, font=("Helvetica", 12))
         self.output_box2.pack(padx=10, pady=5)
@@ -207,7 +205,7 @@ class SimpleGUI:
         # self.paned_window.add(self.output_box2)
 
 
-def run_gui(doc_dict):
+def run_gui(doc_dict, retrieval_system):
     root = tk.Tk()
-    app = SimpleGUI(root, doc_dict)
+    app = SimpleGUI(root, doc_dict, retrieval_system)
     root.mainloop()
