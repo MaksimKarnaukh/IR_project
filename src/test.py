dee96baac4c2dd796cc8865ef5200fba6012d1dd
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2024-01-05 23:56:06 +0100

    test.py comments

--- src/test.py
+++ src/test.py
@@ -28,6 +28,14 @@ def calculatePrecisionAndRecall(expected, retrieved) -> tuple:
 
 
 def calculateF1score(precision, recall) -> float:
+    """
+    F1 score calculation (by formula)
+    F1 = 2*P*R/(P+R)
+    :param precision: P
+    :param recall: R
+    :return: F1 score
+    """
+    # F1 score calculation (by formula)
     F1_score = (2*precision*recall) + (precision+recall)
     return F1_score
 
@@ -43,6 +51,7 @@ def calculateKappa(expected_pairs: dict, retrieved_pairs: dict, nr_of_docs, rele
     """
     relevant_retrieved_pairs = {}
 
+    # sort the retrieved pairs by relevance score
     for key in retrieved_pairs.keys():
         if retrieved_pairs[key] >= relevance_threshold:
             relevant_retrieved_pairs[key] = retrieved_pairs[key]
@@ -51,6 +60,7 @@ def calculateKappa(expected_pairs: dict, retrieved_pairs: dict, nr_of_docs, rele
         else:
             break
 
+    # intersection of expected and retrieved pairs
     intersection = list(set(expected_pairs.keys()).intersection(relevant_retrieved_pairs.keys()))
 
     # number of conflicting and non-conflicting agreements
@@ -68,41 +78,62 @@ def calculateKappa(expected_pairs: dict, retrieved_pairs: dict, nr_of_docs, rele
     c2 = ex_conflict_agreement + total_negative_agreement
     Pe = (((r1/nr_of_docs)*(c1/nr_of_docs))+((r2/nr_of_docs)*(c2/nr_of_docs)))
 
+    # kappa calculation (by formula)
     kappa = (P0 - Pe) / (1 - Pe)
 
     return kappa
 
 
 def testAll():
+    """
+    Test the retrieval system.
+    Print the average precision, recall, F1 score and kappa.
+    """
+    # read the ground truth labels
     ground_truth_labels: dict = read_gt(variables.filepath_path_gt)
 
+    # read the dictionary of documents
     doc_dict = getDocDict(filepath_video_games=variables.filepath_video_games, csv_doc_dict=variables.csv_doc_dict)
 
+    # convert the dictionary of documents to a list of documents and a list of document titles
     documents = list(doc_dict.values())
     document_titles = list(doc_dict.keys())
 
+    # initialize the retrieval system
     retrieval_system = RelatedDocumentsRetrieval(document_titles, documents)
     retrieval_system.initialize_tf_idf_matrix(documents)
 
+    # initialize the retrieval system with scikit-learn
     retrieval_system_scipi = RelatedDocumentsRetrieval(document_titles, documents, use_own_vectorizer=False,
                                                        use_own_cosine_similarity=False)
     retrieval_system_scipi.initialize_tf_idf_matrix(documents)
 
+    # test the retrieval systems
     for retrievalsystem in [retrieval_system, retrieval_system_scipi]:
+        # initialize the metrics
         metrics = {"precisions": [], "recalls": [], "F1 scores": [], "kappas": []}
+        # loop over the ground truth labels
         for title, similar_documents_gt in alive_it(ground_truth_labels.items(), title="Testing"):
+            # retrieve similar documents
             query_document = doc_dict[title]
             similar_documents_titles, similar_documents, scores = retrievalsystem.retrieve_similar_documents(query_document, title, 10)
+            # calculate the metrics
             score_dict = dict(zip(similar_documents_titles, scores))
             par = calculatePrecisionAndRecall(similar_documents_gt, similar_documents_titles)
+            # add the metrics to the dictionary
             metrics["precisions"].append(par[0])
             metrics["recalls"].append(par[1])
+            # calculate the F1 score
             F1 = calculateF1score(par[0], par[1])
+            # add the F1 score to the dictionary
             metrics["F1 scores"].append(F1)
+            # calculate the kappa
             ones = [1] * len(similar_documents_gt)
             scored_gt = dict(zip(similar_documents_gt, ones))
             kappa = calculateKappa(scored_gt, score_dict, nr_of_docs=len(documents))
+            # add the kappa to the dictionary
             metrics["kappas"].append(kappa)
+        # print the metrics
         print('\n')
         print("Average precision: ", sum(metrics["precisions"])/len(metrics["precisions"]))
         print("Average recall: ", sum(metrics["recalls"])/len(metrics["recalls"]))
@@ -110,13 +141,5 @@ def testAll():
         print("Average kappa: ", sum(metrics["kappas"]) / len(metrics["kappas"]))
 
 if __name__ == '__main__':
-    # d = read_gt(variables.filepath_path_gt)  # Read ground truth
-    # # print(d["Assassin's Creed IV: Black Flag"])
-    #
-    # # Wikipedia example: expected output is
-    # # precision = 5/8 = 0.625
-    # # recall = 5/12 = 0.41666...
-    # exp = ['dog0', 'dog1', 'dog2', 'dog3', 'dog4', 'dog5', 'dog6', 'dog7', 'dog8', 'dog9', 'dog10', 'dog11']
-    # ret = ['dog0', 'dog1', 'dog2', 'dog3', 'dog4', "cat1", "cat2", "cat3"]
-    # print(calculatePrecisionAndRecall(exp, ret))
+    # test the retrieval system
     testAll()
