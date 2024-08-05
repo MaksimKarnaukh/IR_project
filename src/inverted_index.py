db7c108d6cb7b3c643c33df699b9577f00dee65a
MaksimKarnaukh <Maksim.Karnaukh@student.uantwerpen.be>
2024-08-05 18:27:45 +0200

    inverted index working with retrieval

--- src/inverted_index.py
+++ src/inverted_index.py
@@ -6,13 +6,26 @@ import pickle
 
 
 class SPIMI:
-    def __init__(self, block_size_limit=100000, output_dir="../output/spimi_output"):
+    def __init__(self, block_size_limit=10000, output_dir="../output/spimi_output"):
         self.block_size_limit = block_size_limit
         self.output_dir = output_dir
         self.block_num = 0
         self.dictionary = dict() # {term: {doc_id: freq}}
         self.token_count = 0
+        self.term_positions = {}  # {term: offset}
         os.makedirs(self.output_dir, exist_ok=True)
+
+    # function for writing the term and postings to disk
+    def write_index_entry(self, term, postings, f):
+        try:
+            term_bytes = term.encode('utf-8')
+            postings_bytes = pickle.dumps(dict(postings))
+            f.write(struct.pack('I', len(term_bytes)))
+            f.write(term_bytes)
+            f.write(struct.pack('I', len(postings_bytes)))
+            f.write(postings_bytes)
+        except Exception as e:
+            print(f"Error writing term '{term}' to disk: {e}")
 
     def write_block_to_disk(self):
         block_filename = os.path.join(self.output_dir, f"block_{self.block_num}.bin")
@@ -40,7 +53,7 @@ class SPIMI:
         postings = pickle.loads(f.read(postings_length))
         return term, postings
 
-    def merge_two_blocks(self, block_file1, block_file2):
+    def merge_two_blocks(self, block_file1, block_file2, is_last_merge=False):
         merged_index = defaultdict(dict)
 
         f1 = open(block_file1, 'rb')
@@ -66,6 +79,7 @@ class SPIMI:
 
         merged_block_filename = os.path.join(self.output_dir, f"merged_{self.block_num}.bin")
         with open(merged_block_filename, 'wb') as f:
+            offset = f.tell()
             for term, postings in merged_index.items():
                 term_bytes = term.encode('utf-8')
                 postings_bytes = pickle.dumps(dict(postings))
@@ -73,24 +87,48 @@ class SPIMI:
                 f.write(term_bytes)
                 f.write(struct.pack('I', len(postings_bytes)))
                 f.write(postings_bytes)
+                if is_last_merge:
+                    self.term_positions[term] = offset
+                    offset = f.tell()
 
         self.block_num += 1
         return merged_block_filename
 
-    def merge_blocks(self, block_files):
+    # def merge_blocks(self, block_files):
+    #     while len(block_files) > 1:
+    #         new_block_files = []
+    #
+    #         for i in range(0, len(block_files), 2):
+    #             if i + 1 < len(block_files):
+    #                 merged_block = self.merge_two_blocks(block_files[i], block_files[i + 1])
+    #                 new_block_files.append(merged_block)
+    #             else:
+    #                 new_block_files.append(block_files[i])
+    #
+    #         block_files = new_block_files
+    #
+    #     return block_files[0]
+
+    def merge_blocks(self, block_files, delete_merged_blocks=True):
         while len(block_files) > 1:
             new_block_files = []
 
             for i in range(0, len(block_files), 2):
                 if i + 1 < len(block_files):
-                    merged_block = self.merge_two_blocks(block_files[i], block_files[i + 1])
+                    merged_block = self.merge_two_blocks(block_files[i], block_files[i + 1], (len(block_files) == 2))
                     new_block_files.append(merged_block)
+                    if delete_merged_blocks:
+                        os.remove(block_files[i])
+                        os.remove(block_files[i + 1])
                 else:
                     new_block_files.append(block_files[i])
 
             block_files = new_block_files
 
-        return block_files[0]
+        final_block = block_files[0]
+        final_index_filename = os.path.join(self.output_dir, 'inverted_index.bin')
+        os.rename(final_block, final_index_filename)
+        return final_index_filename
 
     def spimi_invert(self, token_stream):
         block_files = []
@@ -116,6 +154,20 @@ class SPIMI:
         final_index_filename = self.merge_blocks(block_files)
 
         return final_index_filename
+
+    def get_posting_list(self, term):
+        if term in self.term_positions:
+            offset = self.term_positions[term]
+            block_filename = os.path.join(self.output_dir, 'inverted_index.bin')
+            with open(block_filename, 'rb') as f:
+                f.seek(offset)
+                term_length = struct.unpack('I', f.read(4))[0]
+                f.read(term_length)  # Skip term
+                postings_length = struct.unpack('I', f.read(4))[0]
+                postings = pickle.loads(f.read(postings_length))
+                return postings
+        else:
+            return None
 
 
 def generate_token_stream(documents):
@@ -152,19 +204,6 @@ def print_block_file(file_path):
             for doc_id, freq in postings.items():
                 print(f"    Doc ID: {doc_id}, Frequency: {freq}")
 
-# Example usage
-# if __name__ == "__main__":
-#     documents = [
-#         "the cat in the hat",
-#         "the quick brown fox",
-#         "the lazy dog",
-#         "the hat is in the cat",
-#     ]
-#
-#     token_stream = generate_token_stream(documents)
-#     spimi = SPIMI(block_size_limit=10)
-#     final_index_filename = spimi.spimi_invert(token_stream)
-#     print(f"Final index written to: {final_index_filename}")
 
 # Example usage
 if __name__ == "__main__":
@@ -278,8 +317,16 @@ if __name__ == "__main__":
     # print contents of all binary files in the output/spimi_output directory
     for file in os.listdir('../output/spimi_output'):
         if file.endswith(".bin"):
+            print("-----\n")
             print("\nfile:", file)
             print_block_file(os.path.join('../output/spimi_output', file))
-            print("-----")
-            print("-----")
             print("-----\n")
+
+    # Print contents of the final index file
+    print("\nFinal index contents:")
+    print_block_file(final_index_filename)
+
+    # Test retrieving the posting list for a term
+    term = "then"
+    postings = spimi.get_posting_list(term)
+    print(f"\nPosting list for term '{term}': {postings}")
